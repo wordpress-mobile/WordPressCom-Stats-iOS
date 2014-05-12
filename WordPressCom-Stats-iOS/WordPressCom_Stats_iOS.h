a2dd06a9b54181d4ecba2b001de7cbdbe93b2fe9
Aaron Douglas <astralbodies@gmail.com>
2014-05-12 08:51:40 -0500

    Initial Commit

---
+++ WordPressCom-Stats-iOS/WordPressCom_Stats_iOS.h
@@ -0,0 +1,13 @@
+//
+//  WordPressCom_Stats_iOS.h
+//  WordPressCom-Stats-iOS
+//
+//  Created by Aaron Douglas on 5/12/14.
+//  Copyright (c) 2014 Automattic Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WordPressCom_Stats_iOS : NSObject
+
+@end
---
+++ WordPressCom-Stats-iOSTests/WordPressCom_Stats_iOSTests.m
@@ -0,0 +1,34 @@
+//
+//  WordPressCom_Stats_iOSTests.m
+//  WordPressCom-Stats-iOSTests
+//
+//  Created by Aaron Douglas on 5/12/14.
+//  Copyright (c) 2014 Automattic Inc. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WordPressCom_Stats_iOSTests : XCTestCase
+
+@end
+
+@implementation WordPressCom_Stats_iOSTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
