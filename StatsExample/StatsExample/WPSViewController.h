71b8aecc83dd0d98c2fd304b145daab1e9f93dc5
Aaron Douglas <astralbodies@gmail.com>
2014-06-16 15:36:24 -0500

    Initial Commit

---
+++ StatsExample/StatsExample/WPSViewController.h
@@ -0,0 +1,13 @@
+//
+//  WPSViewController.h
+//  StatsExample
+//
+//  Created by Aaron Douglas on 6/16/14.
+//  Copyright (c) 2014 Automattic Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WPSViewController : UIViewController
+
+@end
---
+++ StatsExample/StatsExample/WPSViewController.m
@@ -0,0 +1,29 @@
+//
+//  WPSViewController.m
+//  StatsExample
+//
+//  Created by Aaron Douglas on 6/16/14.
+//  Copyright (c) 2014 Automattic Inc. All rights reserved.
+//
+
+#import "WPSViewController.h"
+
+@interface WPSViewController ()
+
+@end
+
+@implementation WPSViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
