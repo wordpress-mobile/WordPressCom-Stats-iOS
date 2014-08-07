b1cc3b819441ba7cb5298d911b880c7b610a8402
Aaron Douglas <astralbodies@gmail.com>
2014-08-07 08:36:54 -0500

    Removed AsyncTestHelper and replaced it with XCTestExpectation

--- WordPressCom-Stats-iOSTests/WPStatsServiceRemoteTest.m
+++ WordPressCom-Stats-iOSTests/WPStatsServiceRemoteTest.m
@@ -2,7 +2,6 @@
 #import <OCMock/OCMock.h>
 #import <OHHTTPStubs/OHHTTPStubs.h>
 #import "WPStatsServiceRemote.h"
-#import "AsyncTestHelper.h"
 
 @interface WPStatsServiceRemoteTest : XCTestCase
 {
@@ -27,8 +26,7 @@
 
 - (void)testMarshallingMappingHappens
 {
-    __block BOOL completionCalled = NO;
-    ATHStart();
+    XCTestExpectation *expectation = [self expectationWithDescription:@"Fetch completion"];
     
     [OHHTTPStubs stubRequestsPassingTest:^BOOL(NSURLRequest *request) {
         return [[request.URL absoluteString] hasPrefix:@"https://public-api.wordpress.com/rest/v1/batch"];
@@ -43,7 +41,6 @@
     NSDate *yesterday = [formatter dateFromString:@"2014-05-09"];
     
     [subject fetchStatsForTodayDate:today andYesterdayDate:yesterday withCompletionHandler:^(WPStatsSummary *summary, NSDictionary *topPosts, NSDictionary *clicks, NSDictionary *countryViews, NSDictionary *referrers, NSDictionary *searchTerms, WPStatsViewsVisitors *viewsVisitors) {
-        completionCalled = YES;
         XCTAssertNotNil(summary, @"summary should not be nil.");
         XCTAssertNotNil(topPosts, @"topPosts should not be nil.");
         XCTAssertNotNil(clicks, @"clicks should not be nil.");
@@ -52,15 +49,13 @@
         XCTAssertNotNil(searchTerms, @"searchTerms should not be nil.");
         XCTAssertNotNil(viewsVisitors, @"viewsVisitors should not be nil.");
         
-        ATHNotify();
+        [expectation fulfill];
     } failureHandler:^(NSError *error) {
         XCTFail(@"Failure handler should not be called here.");
-        ATHNotify();
+        [expectation fulfill];
     }];
     
-    ATHEnd();
-    
-    XCTAssertTrue(completionCalled, @"Completion block not called.");
+    [self waitForExpectationsWithTimeout:2.0 handler:nil];
 }
 
 @end
