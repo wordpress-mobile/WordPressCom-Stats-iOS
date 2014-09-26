499c9a89241c2bafd4b12a114e50395033389851
Aaron Douglas <astralbodies@gmail.com>
2014-09-26 07:04:14 -0500

    Made unit test use implicit unwrapped

--- WordPressCom-Stats-iOSTests/WPStatsViewsVisitorsTests.swift
+++ WordPressCom-Stats-iOSTests/WPStatsViewsVisitorsTests.swift
@@ -2,7 +2,7 @@ import UIKit
 import XCTest
 
 class WPStatsViewsVisitorsTests: XCTestCase {
-    var viewsVisitors: WPStatsViewsVisitors?
+    var viewsVisitors: WPStatsViewsVisitors!
 
     override func setUp() {
         super.setUp()
@@ -16,42 +16,42 @@ class WPStatsViewsVisitorsTests: XCTestCase {
     }
     
     func testNicePointNamesEmptyText() {
-        let result = viewsVisitors!.nicePointNames("", forUnit: WPStatsViewsVisitorsUnit.StatsViewsVisitorsUnitDay)
+        let result = viewsVisitors.nicePointNames("", forUnit: WPStatsViewsVisitorsUnit.StatsViewsVisitorsUnitDay)
         
         XCTAssertNotNil(result, "Result shouldn't be nil")
         XCTAssertTrue(result.count == 0, "Should have no entry")
     }
     
     func testNicePointNamesNilText() {
-        let result = viewsVisitors!.nicePointNames(nil, forUnit: WPStatsViewsVisitorsUnit.StatsViewsVisitorsUnitDay)
+        let result = viewsVisitors.nicePointNames(nil, forUnit: WPStatsViewsVisitorsUnit.StatsViewsVisitorsUnitDay)
         
         XCTAssertNotNil(result, "Result shouldn't be nil")
         XCTAssertTrue(result.count == 0, "Should have no entry")
     }
     
     func testNicePointNamesBlankText() {
-        let result = viewsVisitors!.nicePointNames(" ", forUnit: WPStatsViewsVisitorsUnit.StatsViewsVisitorsUnitDay)
+        let result = viewsVisitors.nicePointNames(" ", forUnit: WPStatsViewsVisitorsUnit.StatsViewsVisitorsUnitDay)
         
         XCTAssertNotNil(result, "Result shouldn't be nil")
         XCTAssertTrue(result.count == 1, "Should have one entry")
     }
     
     func testNicePointNamesNonDateTextDay() {
-        let result = viewsVisitors!.nicePointNames("This isn't a date", forUnit: WPStatsViewsVisitorsUnit.StatsViewsVisitorsUnitDay)
+        let result = viewsVisitors.nicePointNames("This isn't a date", forUnit: WPStatsViewsVisitorsUnit.StatsViewsVisitorsUnitDay)
         
         XCTAssertNotNil(result, "Result shouldn't be nil")
         XCTAssertTrue(result.count == 1, "Should have one entry")
     }
     
     func testNicePointNamesNonDateTextDayMonth() {
-        let result = viewsVisitors!.nicePointNames("This isn't a date", forUnit: WPStatsViewsVisitorsUnit.StatsViewsVisitorsUnitMonth)
+        let result = viewsVisitors.nicePointNames("This isn't a date", forUnit: WPStatsViewsVisitorsUnit.StatsViewsVisitorsUnitMonth)
         
         XCTAssertNotNil(result, "Result shouldn't be nil")
         XCTAssertTrue(result.count == 1, "Should have one entry")
     }
     
     func testNicePointNamesNonDateTextWeek() {
-        let result = viewsVisitors!.nicePointNames("This isn't a date", forUnit: WPStatsViewsVisitorsUnit.StatsViewsVisitorsUnitWeek)
+        let result = viewsVisitors.nicePointNames("This isn't a date", forUnit: WPStatsViewsVisitorsUnit.StatsViewsVisitorsUnitWeek)
         
         XCTAssertNotNil(result, "Result shouldn't be nil")
         XCTAssertTrue(result.count == 1, "Should have one entry")
