d9ef93bac6657f4ded87a9120f5a130eaab4083c
Aaron Douglas <astralbodies@gmail.com>
2014-12-23 07:37:01 -0600

    Added moreItems boolean to data model for a group

--- WordPressCom-Stats-iOS/StatsGroup.h
+++ WordPressCom-Stats-iOS/StatsGroup.h
@@ -3,6 +3,7 @@
 @interface StatsGroup : NSObject
 
 @property (nonatomic, strong)   NSArray *items; // StatsItem
+@property (nonatomic, assign)   BOOL moreItemsExist;
 @property (nonatomic, copy)     NSString *titlePrimary;
 @property (nonatomic, copy)     NSString *titleSecondary;
 @property (nonatomic, strong)   NSURL *iconUrl;
