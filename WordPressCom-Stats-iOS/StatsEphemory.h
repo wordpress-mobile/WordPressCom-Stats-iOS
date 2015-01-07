0a8dc16e00ec73ca27c8960fc77272a6d0571959
Aaron Douglas <astralbodies@gmail.com>
2015-01-07 13:24:46 -0600

    Added class documentation header to help with class name obscurity

--- WordPressCom-Stats-iOS/StatsEphemory.h
+++ WordPressCom-Stats-iOS/StatsEphemory.h
@@ -1,5 +1,12 @@
 #import <Foundation/Foundation.h>
 
+/*!
+    @class      StatsEphemory
+    @brief      A lightweight container for NSCache.
+    @discussion Encapsulates an NSCache instance with an additional parameter for expiration time interval.
+ 
+                This class' name is a portmanteua of Ephemeral and Memory.
+*/
 @interface StatsEphemory : NSObject
 
 @property (nonatomic, assign, readonly) NSTimeInterval expiryInterval;
@@ -9,7 +16,6 @@
 - (id)objectForKey:(id)key;
 - (void)setObject:(id)obj forKey:(id)key;
 - (void)removeObjectForKey:(id)key;
-
 - (void)removeAllObjects;
 
 @end
