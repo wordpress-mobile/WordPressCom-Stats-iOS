6c8b7a777a5653e82f6423fe25dd59bffdab49d1
Aaron Douglas <astralbodies@gmail.com>
2015-02-04 13:47:05 -0600

    Remove comment from header

--- WordPressCom-Stats-iOS/StatsStandardBorderedTableViewCell.h
+++ WordPressCom-Stats-iOS/StatsStandardBorderedTableViewCell.h
@@ -1,11 +1,3 @@
-//
-//  StatsStandardBorderedTableViewCell.h
-//  WordPressCom-Stats-iOS
-//
-//  Created by Aaron Douglas on 1/21/15.
-//  Copyright (c) 2015 Automattic Inc. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface StatsStandardBorderedTableViewCell : UITableViewCell
