5c265c34764546d7d41984653aea74f59fa396f9
Aaron Douglas <astralbodies@gmail.com>
2015-02-04 13:48:16 -0600

    Indent to follow suit of WPTableViewCell - hacky but whatevs

--- WordPressCom-Stats-iOS/StatsTableSectionHeaderView.m
+++ WordPressCom-Stats-iOS/StatsTableSectionHeaderView.m
@@ -1,5 +1,6 @@
 #import "StatsTableSectionHeaderView.h"
 #import "WPStyleGuide+Stats.h"
+#import <WPTableViewCell.h>
 
 @interface StatsTableSectionHeaderView ()
 
@@ -8,6 +9,7 @@
 @end
 
 @implementation StatsTableSectionHeaderView
+
 
 - (instancetype)initWithReuseIdentifier:(NSString *)reuseIdentifier
 {
@@ -19,8 +21,28 @@
         _theBoxView.backgroundColor = [UIColor colorWithRed:210.0/255.0 green:222.0/255.0 blue:238.0/255.0 alpha:1.0];
         [self.contentView addSubview:_theBoxView];
         
+        [self setClipsToBounds:YES];
     }
     return self;
+}
+
+
+- (void)setFrame:(CGRect)frame {
+    CGFloat width = self.superview.frame.size.width;
+    // On iPad, add a margin around tables
+    if (IS_IPAD && width > WPTableViewFixedWidth) {
+        CGFloat x = (width - WPTableViewFixedWidth) / 2;
+        // If origin.x is not equal to x we add the value.
+        // This is a semi-fix / work around for an issue positioning cells on
+        // iOS 8 when editing a table view and the delete button is visible.
+        if (x != frame.origin.x) {
+            frame.origin.x += x;
+        } else {
+            frame.origin.x = x;
+        }
+        frame.size.width = WPTableViewFixedWidth;
+    }
+    [super setFrame:frame];
 }
 
 
@@ -28,8 +50,17 @@
 {
     [super layoutSubviews];
     
+    // Need to set the origin again on iPad (for margins)
+    CGFloat width = self.superview.frame.size.width;
+    if (IS_IPAD && width > WPTableViewFixedWidth) {
+        CGRect frame = self.frame;
+        frame.origin.x = (width - WPTableViewFixedWidth) / 2;
+        self.frame = frame;
+    }
+
     CGFloat borderSidePadding = IS_IPHONE ? RPTVCHorizontalOuterPadding - 1.0f : 0.0f; // Just to the left of the container
     self.theBoxView.frame = CGRectMake(borderSidePadding, self.isFooter ? -1.0f : 0.0f, CGRectGetWidth(self.frame) - 2 * borderSidePadding, 1.0f);
 }
+
 
 @end
