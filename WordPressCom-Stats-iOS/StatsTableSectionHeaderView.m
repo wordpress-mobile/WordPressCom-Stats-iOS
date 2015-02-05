434de9900de45b3a6574324d3f11686ce8025469
Aaron Douglas <astralbodies@gmail.com>
2015-02-05 08:12:30 -0600

    Fixed layout problems due to clipping and cell reuse not resetting vals

--- WordPressCom-Stats-iOS/StatsTableSectionHeaderView.m
+++ WordPressCom-Stats-iOS/StatsTableSectionHeaderView.m
@@ -20,8 +20,6 @@
         _theBoxView = [[UIView alloc] initWithFrame:CGRectZero];
         _theBoxView.backgroundColor = [UIColor colorWithRed:210.0/255.0 green:222.0/255.0 blue:238.0/255.0 alpha:1.0];
         [self.contentView addSubview:_theBoxView];
-        
-        [self setClipsToBounds:YES];
     }
     return self;
 }
@@ -59,7 +57,13 @@
     }
 
     CGFloat borderSidePadding = RPTVCHorizontalOuterPadding - 1.0f; // Just to the left of the container
-    self.theBoxView.frame = CGRectMake(borderSidePadding, self.isFooter ? -1.0f : 0.0f, CGRectGetWidth(self.frame) - 2 * borderSidePadding, 1.0f);
+    self.theBoxView.frame = CGRectMake(borderSidePadding, self.isFooter ? -1.0f : 0.0f, CGRectGetWidth(self.frame) - 2.0 * borderSidePadding, 1.0f);
+}
+
+- (void)prepareForReuse
+{
+    [super prepareForReuse];
+    self.footer = NO;
 }
 
 
