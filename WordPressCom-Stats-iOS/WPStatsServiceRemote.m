9614ea3d98c0a0739072262f8c64137198701932
Aaron Douglas <astralbodies@gmail.com>
2015-02-06 13:58:59 -0600

    Increase flag icons to 60x60 for best appearance

--- WordPressCom-Stats-iOS/WPStatsServiceRemote.m
+++ WordPressCom-Stats-iOS/WPStatsServiceRemote.m
@@ -900,22 +900,22 @@ followersEmailCompletionHandler:(StatsRemoteItemsCompletion)followersEmailComple
             
             if ([serviceID isEqualToString:@"facebook"]) {
                 serviceLabel = @"Facebook";
-                iconURL = [NSURL URLWithString:@"https://secure.gravatar.com/blavatar/2343ec78a04c6ea9d80806345d31fd78?s=48"];
+                iconURL = [NSURL URLWithString:@"https://secure.gravatar.com/blavatar/2343ec78a04c6ea9d80806345d31fd78?s=60"];
             } else if ([serviceID isEqualToString:@"twitter"]) {
                 serviceLabel = @"Twitter";
-                iconURL = [NSURL URLWithString:@"https://secure.gravatar.com/blavatar/7905d1c4e12c54933a44d19fcd5f9356?s=48"];
+                iconURL = [NSURL URLWithString:@"https://secure.gravatar.com/blavatar/7905d1c4e12c54933a44d19fcd5f9356?s=60"];
             } else if ([serviceID isEqualToString:@"tumblr"]) {
                 serviceLabel = @"Tumblr";
-                iconURL = [NSURL URLWithString:@"https://secure.gravatar.com/blavatar/84314f01e87cb656ba5f382d22d85134?s=48"];
+                iconURL = [NSURL URLWithString:@"https://secure.gravatar.com/blavatar/84314f01e87cb656ba5f382d22d85134?s=60"];
             } else if ([serviceID isEqualToString:@"google_plus"]) {
                 serviceLabel = @"Google+";
-                iconURL = [NSURL URLWithString:@"https://secure.gravatar.com/blavatar/4a4788c1dfc396b1f86355b274cc26b3?s=48"];
+                iconURL = [NSURL URLWithString:@"https://secure.gravatar.com/blavatar/4a4788c1dfc396b1f86355b274cc26b3?s=60"];
             } else if ([serviceID isEqualToString:@"linkedin"]) {
                 serviceLabel = @"LinkedIn";
-                iconURL = [NSURL URLWithString:@"https://secure.gravatar.com/blavatar/f54db463750940e0e7f7630fe327845e?s=48"];
+                iconURL = [NSURL URLWithString:@"https://secure.gravatar.com/blavatar/f54db463750940e0e7f7630fe327845e?s=60"];
             } else if ([serviceID isEqualToString:@"path"]) {
                 serviceLabel = @"Path";
-                iconURL = [NSURL URLWithString:@"https://secure.gravatar.com/blavatar/3a03c8ce5bf1271fb3760bb6e79b02c1?s=48"];
+                iconURL = [NSURL URLWithString:@"https://secure.gravatar.com/blavatar/3a03c8ce5bf1271fb3760bb6e79b02c1?s=60"];
             }
             
             statsItem.label = serviceLabel;
