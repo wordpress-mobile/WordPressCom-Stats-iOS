94f071428cf35be111d4d96be13b5426e10f4375
Aaron Douglas <astralbodies@gmail.com>
2015-03-06 14:37:35 -0600

    Fixed section count when data is loading
    Really wasn't visible until I tested on the iPad.

--- WordPressCom-Stats-iOS/StatsPostDetailsTableViewController.m
+++ WordPressCom-Stats-iOS/StatsPostDetailsTableViewController.m
@@ -81,7 +81,7 @@ static NSString *const StatsTableNoResultsCellIdentifier = @"NoResultsRow";
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return 4;
+    return 4 + (self.isRefreshing ? 1 : 0);
 }
 
 
