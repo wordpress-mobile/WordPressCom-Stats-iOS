8fe6673694858512dd2cff2c3442dafb6db90fa9
Aaron Douglas <astralbodies@gmail.com>
2015-05-15 06:49:54 -0500

    Added stats insights data model

---
+++ WordPressCom-Stats-iOS/StatsInsights.h
@@ -0,0 +1,13 @@
+//
+//  StatsInsights.h
+//  WordPressCom-Stats-iOS
+//
+//  Created by Aaron Douglas on 5/14/15.
+//  Copyright (c) 2015 Automattic Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface StatsInsights : NSObject
+
+@end
---
+++ WordPressCom-Stats-iOS/StatsInsights.m
@@ -0,0 +1,13 @@
+//
+//  StatsInsights.m
+//  WordPressCom-Stats-iOS
+//
+//  Created by Aaron Douglas on 5/14/15.
+//  Copyright (c) 2015 Automattic Inc. All rights reserved.
+//
+
+#import "StatsInsights.h"
+
+@implementation StatsInsights
+
+@end
