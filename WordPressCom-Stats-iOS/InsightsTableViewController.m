11400d022114fe89d0c04af9a888e07c1dfdf277
Aaron Douglas <astralbodies@gmail.com>
2015-05-29 07:38:54 -0500

    Removed attributed strings

--- WordPressCom-Stats-iOS/InsightsTableViewController.m
+++ WordPressCom-Stats-iOS/InsightsTableViewController.m
@@ -104,41 +104,6 @@
     self.todayCommentsValueLabel.text = @"--";
     self.todayCommentsValueLabel.textColor = [WPStyleGuide grey];
 
-//    NSAttributedString *space = [[NSAttributedString alloc] initWithString:@" " attributes:@{NSFontAttributeName : [WPFontManager openSansRegularFontOfSize:10.0f]}];
-//
-//    // All Time Posts label
-//    NSMutableAttributedString *allTimePosts = [[NSMutableAttributedString alloc] init];
-//    NSAttributedString *postsIcon = [[NSAttributedString alloc] initWithString:@"" attributes:@{NSFontAttributeName : [WPFontManager noticonsReguarFontOfSize:16.0f], NSBaselineOffsetAttributeName : @(-4.0f)}];
-//    NSAttributedString *postsLabel = [[NSAttributedString alloc] initWithString:[NSLocalizedString(@"Posts", @"Posts label") uppercaseStringWithLocale:[NSLocale currentLocale]] attributes:@{NSFontAttributeName : [WPFontManager openSansRegularFontOfSize:10.0f]}];
-//    [allTimePosts appendAttributedString:postsIcon];
-//    [allTimePosts appendAttributedString:postsLabel];
-//    self.allTimePostsLabel.attributedText = allTimePosts;
-//    
-//    // All Time Views label
-//    NSMutableAttributedString *allTimeViews = [[NSMutableAttributedString alloc] init];
-//    NSAttributedString *viewsIcon = [[NSAttributedString alloc] initWithString:@"" attributes:@{NSFontAttributeName : [WPFontManager noticonsReguarFontOfSize:16.0f], NSBaselineOffsetAttributeName : @(-4.0f)}];
-//    NSAttributedString *viewsLabel = [[NSAttributedString alloc] initWithString:[NSLocalizedString(@"Views", @"Views label") uppercaseStringWithLocale:[NSLocale currentLocale]] attributes:@{NSFontAttributeName : [WPFontManager openSansRegularFontOfSize:10.0f]}];
-//    [allTimeViews appendAttributedString:viewsIcon];
-//    [allTimeViews appendAttributedString:space];
-//    [allTimeViews appendAttributedString:viewsLabel];
-//    self.allTimeViewsLabel.attributedText = allTimeViews;
-//
-//    // All Time Visitors label
-//    NSMutableAttributedString *allTimeVisitors = [[NSMutableAttributedString alloc] init];
-//    NSAttributedString *visitorsIcon = [[NSAttributedString alloc] initWithString:@"" attributes:@{NSFontAttributeName : [WPFontManager noticonsReguarFontOfSize:16.0f], NSBaselineOffsetAttributeName : @(-4.0f)}];
-//    NSAttributedString *visitorsLabel = [[NSAttributedString alloc] initWithString:[NSLocalizedString(@"Visitors", @"Visitors label") uppercaseStringWithLocale:[NSLocale currentLocale]] attributes:@{NSFontAttributeName : [WPFontManager openSansRegularFontOfSize:10.0f]}];
-//    [allTimeVisitors appendAttributedString:visitorsIcon];
-//    [allTimeVisitors appendAttributedString:visitorsLabel];
-//    self.allTimeVisitorsLabel.attributedText = allTimeVisitors;
-//
-//    // All Time Best Views label
-//    NSMutableAttributedString *allTimeBestViews = [[NSMutableAttributedString alloc] init];
-//    NSAttributedString *bestViewsIcon = [[NSAttributedString alloc] initWithString:@"" attributes:@{NSFontAttributeName : [WPFontManager noticonsReguarFontOfSize:16.0f], NSBaselineOffsetAttributeName : @(-4.0f)}];
-//    NSAttributedString *bestViewsLabel = [[NSAttributedString alloc] initWithString:[NSLocalizedString(@"Best Views Ever", @"Best Views Ever label") uppercaseStringWithLocale:[NSLocale currentLocale]] attributes:@{NSFontAttributeName : [WPFontManager openSansRegularFontOfSize:10.0f]}];
-//    [allTimeBestViews appendAttributedString:bestViewsIcon];
-//    [allTimeBestViews appendAttributedString:space];
-//    [allTimeBestViews appendAttributedString:bestViewsLabel];
-//    self.allTimeBestViewsLabel.attributedText = allTimeBestViews;
     [self retrieveStats];
 }
 
