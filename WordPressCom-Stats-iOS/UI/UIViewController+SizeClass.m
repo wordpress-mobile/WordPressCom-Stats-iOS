48763e7b0ff85987e0152f1f540ad7c126c87325
Aaron Douglas <astralbodies@gmail.com>
2015-12-30 09:39:09 -0600

    Killed iOS 7 support in size class extension

--- WordPressCom-Stats-iOS/UI/UIViewController+SizeClass.m
+++ WordPressCom-Stats-iOS/UI/UIViewController+SizeClass.m
@@ -4,12 +4,6 @@
 
 - (BOOL)isViewHorizontallyCompact
 {
-    // iOS <= 8:
-    // We'll just consider 'Compact' all of non iPad Devices
-    if ([self respondsToSelector:@selector(traitCollection)] == false) {
-        return ([UIDevice currentDevice].userInterfaceIdiom == UIUserInterfaceIdiomPad) == false;
-    }
-    
     return self.traitCollection.horizontalSizeClass == UIUserInterfaceSizeClassCompact;
 }
 
